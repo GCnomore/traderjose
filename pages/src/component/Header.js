41dfdcbd4c67d363ae278bef8d1b36244a25f20a
isaac <isaacgc08@gmail.com>
2021-03-01 16:03:17 -0800

    Testing vercel

--- pages/src/component/Header.js
+++ pages/src/component/Header.js
@@ -86,8 +86,6 @@ export default function Header() {
       const res = await axios.get(
         `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latlng}&key=${API_KEY}`
       );
-      console.log(API_KEY);
-      console.log(res.data);
       setLocation(res.data.results[6].formatted_address);
     }
   };
